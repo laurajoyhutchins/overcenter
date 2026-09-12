fa3a0013604d8a6003fa5fb111e8221b3810f2b6
Laura <laurajoyhutchins@gmail.com>
2026-09-12 16:02:15 -0600

    schema: preserve project-transition acquisition request hash

--- migrations/062_project_transition_canonical_authority.sql
+++ migrations/062_project_transition_canonical_authority.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 ALTER TABLE execution_state
-  ADD COLUMN IF NOT EXISTS authority_derivation text;
+  ADD COLUMN IF NOT EXISTS authority_derivation text,
+  ADD COLUMN IF NOT EXISTS acquire_request_hash text;
 
 COMMIT;
